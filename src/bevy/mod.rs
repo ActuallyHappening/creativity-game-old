ce4c561f45d63b6abd912bc91ad93c2312e3e925
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-09-23 16:09:59 +1000

    add: setup

--- src/bevy/mod.rs
+++ src/bevy/mod.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
 
+mod player;
+mod setup;
+
 pub struct MainPlugin;
 impl Plugin for MainPlugin {
 	fn build(&self, _app: &mut App) {}
---
+++ src/bevy/setup.rs
@@ -0,0 +1,62 @@
+use bevy::prelude::*;
+use bevy_mod_picking::{
+	prelude::{
+		Click, RaycastPickCamera, RaycastPickTarget,
+	},
+	PickableBundle,
+};
+
+const CAMERA_HEIGHT: f32 = 100.;
+const LIGHT_HEIGHT: f32 = 100.;
+
+pub struct SetupPlugin;
+impl Plugin for SetupPlugin {
+	fn build(&self, app: &mut App) {
+		app.add_systems(Startup, setup);
+	}
+}
+
+#[derive(Component)]
+struct MainCamera;
+
+pub fn setup(
+	mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>,
+	mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+	// cam
+	commands.spawn((
+		Camera3dBundle {
+			transform: Transform::from_xyz(0., CAMERA_HEIGHT, 0.)
+				.with_rotation(Quat::from_rotation_x(-90_f32.to_radians())),
+			..default()
+		},
+		RaycastPickCamera::default(),
+		MainCamera,
+	));
+
+	// light
+	commands.spawn(PointLightBundle {
+		point_light: PointLight {
+			intensity: 50000.0,
+			range: 250.,
+			// shadows_enabled: true,
+			..default()
+		},
+		transform: Transform::from_xyz(0., LIGHT_HEIGHT, 0.),
+		..default()
+	});
+
+	// ground plane
+	commands.spawn((
+		PbrBundle {
+			mesh: meshes.add(shape::Plane::from_size(500.0).into()),
+			material: materials.add(Color::SILVER.into()),
+			// transform to be behind, xy plane
+			transform: Transform::from_xyz(0., 0., 0.),
+			..default()
+		},
+		PickableBundle::default(),    // Makes the entity pickable
+		RaycastPickTarget::default(), // Marker for the `bevy_picking_raycast` backend
+	));
+}
