7c5c8123b9e0d1bdb78c6658f0b5b4544c1428b5
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-09-24 12:17:29 +1000

    add: initial impl_pixel_type macro

--- src/bevy/world/resources.rs
+++ src/bevy/world/resources.rs
@@ -6,8 +6,7 @@ use rand::Rng;
 pub struct WorldResourcesPlugin;
 impl Plugin for WorldResourcesPlugin {
 	fn build(&self, app: &mut App) {
-		app
-			.add_systems(Startup, initialize_world);
+		app.add_systems(Startup, initialize_world);
 	}
 }
 
@@ -30,4 +29,48 @@ fn generate_random_resource((meshs, mats, _): &mut MMA) -> PbrBundle {
 		transform: Transform::from_xyz(x, 0., z),
 		..Default::default()
 	}
+}
+
+pub struct RCopper;
+struct RDirt;
+
+pub trait IsPixel {
+	const COLOUR: Color;
+}
+
+/// Whether the pixel can be collected and / or used to build things
+pub trait IsResource: IsPixel {}
+
+pub trait NaturallyOccurring: IsPixel {
+	const FREQUENCY: f32;
+}
+
+/// Macro for implementing `IsResource` for a list of types
+macro_rules! impl_pixel_type {
+	($type:ty {
+		col = $col:expr,
+	}) => {
+		impl IsPixel for $type {
+			const COLOUR: Color = $col;
+		}
+	};
+
+	($type:ty {
+		col = $col:expr,
+		res = {}
+	}) => {
+		impl IsPixel for $type {
+			const COLOUR: Color = $col;
+		}
+
+		impl IsResource for $type {}
+	};
+}
+
+impl_pixel_type!(
+	RCopper { col = Color::GREEN, res = {} }
+);
+
+pub struct Resource<R: IsResource> {
+	resource: R,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![feature(const_trait_impl)]
 
 mod tools;
 pub use tools::init_debug_tools;
