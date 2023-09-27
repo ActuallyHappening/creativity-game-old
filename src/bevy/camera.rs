d6999d7db64486173c015b26b59603363251c1ae
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-09-27 12:05:46 +1000

    add: initial orbit code

--- src/bevy/camera.rs
+++ src/bevy/camera.rs
@@ -7,10 +7,15 @@ use bevy_mod_picking::prelude::RaycastPickCamera;
 use super::player::MainPlayer;
 use crate::utils::*;
 
+mod orbit;
+
 pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
 	fn build(&self, app: &mut App) {
-		app.add_systems(Update, Dolly::<MainCamera>::update_active);
+		app.add_systems(
+			Update,
+			(Dolly::<MainCamera>::update_active, orbit::pan_orbit_camera),
+		);
 	}
 }
 
@@ -20,14 +25,13 @@ pub struct MainCamera;
 lazy_static::lazy_static! {
 	static ref INITIAL_ROT: Quat = Quat::from_rotation_x(-45f32.to_radians());
 }
+const ARM: Vec3 = Vec3::new(0., CAMERA_HEIGHT, CAMERA_HEIGHT);
 
 impl CameraPlugin {
 	/// Returns the default camera
 	pub fn default() -> impl Bundle {
-		let initial_pos = Vec3::new(0., CAMERA_HEIGHT, 0.);
 		(
 			Camera3dBundle {
-				transform: Transform::from_translation(initial_pos),
 				camera_3d: Camera3d {
 					// gives the black background of space
 					clear_color: ClearColorConfig::Custom(Color::BLACK),
@@ -38,27 +42,32 @@ impl CameraPlugin {
 			Rig::builder()
 				.with(Position::new(Vec3::ZERO))
 				.with(Rotation::new(*INITIAL_ROT))
-				.with(Arm::new(Vec3::new(0., CAMERA_HEIGHT, CAMERA_HEIGHT)))
+				.with(Arm::new(ARM))
 				// .with(
 				// 	LookAt::new(Vec3::ZERO)
 				// 		.tracking_predictive(false)
 				// 		.tracking_smoothness(0.),
 				// )
-				.with(RotationArm::new(*INITIAL_ROT))
+				.with(RotationArm::<1>::new(*INITIAL_ROT))
+				.with(RotationArm::<2>::new(Quat::IDENTITY))
 				// .with(Smooth::new_position(0.75).predictive(true))
 				.build(),
 			RaycastPickCamera::default(),
 			MainCamera,
+			orbit::PanOrbitCamera {
+				radius: ARM.length(),
+				..default()
+			},
 		)
 	}
 }
 
 #[derive(Debug, Constructor)]
-struct RotationArm {
+struct RotationArm<const N: usize> {
 	offset: Quat,
 }
 
-impl RigDriver for RotationArm {
+impl<const N: usize> RigDriver for RotationArm<N> {
 	fn update(&mut self, params: bevy_dolly::dolly::rig::RigUpdateParams) -> Transform {
 		Transform {
 			rotation: params.parent.rotation.mul_quat(self.offset),
