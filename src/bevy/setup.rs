0f317b3bd3d8708684ac2a9752ef667cc5ff1e17
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-09-29 21:07:33 +1000

    fix: stars now blink better

--- src/bevy/setup.rs
+++ src/bevy/setup.rs
@@ -74,7 +74,7 @@ pub fn setup(mut commands: Commands, mut mma: MMA) {
 	});
 
 	// stars
-	for _ in 0..10000 {
+	for _ in 0..10_000 {
 		stars::spawn_random_star(&mut commands, &mut mma);
 		// tracing::info!("Spawned star");
 	}
--- src/bevy/setup/stars.rs
+++ src/bevy/setup/stars.rs
@@ -2,9 +2,11 @@ use std::ops::Mul;
 
 use crate::utils::*;
 
-#[derive(Component)]
+#[derive(Component, Debug, Clone)]
 pub struct Star {
+	/// Smaller, the lgner the blink durationl
 	blink_speed: f32,
+	/// Between [0, 1] for 0 = no blinking at all and 1 = full blinking
 	blink_strength: f32,
 }
 
@@ -22,24 +24,28 @@ pub fn spawn_random_star(commands: &mut Commands, MMA { meshs, mats, .. }: &mut 
 	// 		..default()
 	// 	})
 	// 	.with_children(|commands| {
-	commands.spawn(PbrBundle {
-		material: mats.add(StandardMaterial {
-			base_color: Color::WHITE,
-			emissive: Color::WHITE,
-			unlit: true,
+	commands.spawn(
+		PbrBundle {
+			material: mats.add(StandardMaterial {
+				base_color: Color::WHITE.with_a(0.1),
+				emissive: Color::WHITE,
+				unlit: true,
+				alpha_mode: AlphaMode::Add,
+				..default()
+			}),
+			mesh: meshs.add(
+				shape::Icosphere {
+					radius: 150.0,
+					subdivisions: 4,
+				}
+				.try_into()
+				.unwrap(),
+			),
+			transform: Transform::from_translation(random_pos()),
 			..default()
-		}),
-		mesh: meshs.add(
-			shape::Icosphere {
-				radius: 150.0,
-				subdivisions: 4,
-			}
-			.try_into()
-			.unwrap(),
-		),
-		transform: Transform::from_translation(random_pos()),
-		..default()
-	});
+		}
+		.insert(Star::new()),
+	);
 	// });
 }
 
@@ -71,6 +77,7 @@ impl Vec3 {
 
 #[derive(Default)]
 pub struct StarMaterials(HashMap<u32, Handle<StandardMaterial>>);
+const PRECISION: u32 = 1000;
 
 pub fn blink_stars(
 	mut mats: Local<StarMaterials>,
@@ -80,16 +87,23 @@ pub fn blink_stars(
 ) {
 	let seconds_since_start = time.elapsed_seconds_wrapped();
 	for (star, mut mat) in stars.iter_mut() {
-		let val = (seconds_since_start / star.blink_speed)
+		let val = (seconds_since_start / (1. / star.blink_speed))
 			.sin()
+			.add(1.)
+			.div(2.)
 			.mul(star.blink_strength)
+			.mul(PRECISION as f32)
 			.round() as u32;
+
+		// debug!("Val: {}", val);
+
 		let handle = if let Some(handle) = mats.0.get(&val) {
 			handle.clone()
 		} else {
 			let handle = mma.mats.add(StandardMaterial {
-				base_color: Color::WHITE,
+				base_color: Color::WHITE.with_a(val as f32 / PRECISION as f32),
 				emissive: Color::WHITE,
+				alpha_mode: AlphaMode::Add,
 				unlit: true,
 				..default()
 			});
@@ -97,5 +111,16 @@ pub fn blink_stars(
 			handle
 		};
 		*mat = handle;
+	}
+}
+
+impl Star {
+	pub fn new() -> Star {
+		let mut rng = rand::thread_rng();
+		
+		Star {
+			blink_speed: rng.gen_range(0.01..0.5),
+			blink_strength: rng.gen_range(0.1..1.),
+		}
 	}
 }
