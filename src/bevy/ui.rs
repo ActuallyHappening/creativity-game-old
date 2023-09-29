0dae7e53f3f6649b15f9329a41323d0d64b88fa0
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-09-29 12:30:01 +1000

    feat: generic cam impl in place

--- src/bevy/ui.rs
+++ src/bevy/ui.rs
@@ -1,71 +1,35 @@
-use crate::{bevy::camera::MainCamera, utils::*};
+use crate::utils::*;
 
 pub struct UiPlugin;
 impl Plugin for UiPlugin {
 	fn build(&self, app: &mut App) {
 		app
-			.add_systems(Startup, setup_ui)
-			.add_systems(Update, update_ui);
+			.add_systems(Startup, setup_camera::<BottomLeft>)
+			.add_systems(Update, update_camera::<BottomLeft>);
 	}
 }
 
 mod camtype;
 pub use camtype::*;
 
-fn setup_ui(mut commands: Commands, mut mma: MM2) {
-	commands.spawn(
-		Camera2dBundle {
-			camera: Camera {
-				order: 1,
-				..default()
-			},
-			camera_2d: Camera2d {
-				clear_color: ClearColorConfig::None,
-			},
-			..default()
-		}
-		.insert(UiCamera::<BottomLeft>::default())
-		.not_pickable(),
-	);
-
-	// Circle
-	commands.spawn(MaterialMesh2dBundle {
-		mesh: mma.meshs.add(shape::Circle::new(50.).into()).into(),
-		material: mma.mats.add(ColorMaterial::from(Color::PURPLE)),
-		transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
-		..default()
-	});
+fn setup_camera<T: CamType>(mut commands: Commands) {
+	commands.spawn(UiCamera::<T>::get_camera_bundle());
 }
 
-fn update_ui() {}
+fn update_camera<T: CamType>(
+	windows: Query<&Window>,
+	mut resize_events: EventReader<WindowResized>,
+	mut cam: Query<&mut Transform, With<UiCamera<T>>>,
+) {
+	for ev in resize_events.iter() {
+		let window = windows.get(ev.window).unwrap();
+		let mut cam = cam.single_mut();
 
-// struct ItemPreview;
-// impl ItemPreview {
-// 	const WIDTH_PERCENT: u32 = 20;
-// 	const MARGIN: u32 = 20;
-// 	const EDGE_OFFSET: u32 = 0;
-// }
+		let width = window.resolution.width();
+		let height = window.resolution.height();
 
-// fn update_ui(
-// 	windows: Query<&Window>,
-// 	mut resize_events: EventReader<WindowResized>,
-// 	mut cam: Query<&mut Camera, (With<UiCamera>, Without<MainCamera>)>,
-// ) {
-// 	for resize_event in resize_events.iter() {
-// 		let window = windows.get(resize_event.window).unwrap();
-// 		let mut preview_cam = cam.single_mut();
-
-// 		let width = window.resolution.physical_width();
-// 		let _height = window.resolution.physical_height();
-// 		let preview_width = ((ItemPreview::WIDTH_PERCENT as f32 / 100.) * width as f32).round() as u32
-// 			- ItemPreview::MARGIN * 2;
-// 		let top_left_x = width - preview_width - ItemPreview::MARGIN - ItemPreview::EDGE_OFFSET;
-// 		let top_left_y = ItemPreview::MARGIN + ItemPreview::EDGE_OFFSET;
-
-// 		preview_cam.viewport = Some(Viewport {
-// 			physical_position: UVec2::new(top_left_x, top_left_y),
-// 			physical_size: UVec2::new(preview_width, preview_width),
-// 			..default()
-// 		});
-// 	}
-// }
+		let cam_translation = T::get_cam_transform(width / 2., height / 2.);
+		info!("Setting cam translation to {:?}", cam_translation);
+		cam.translation = Vec3::new(cam_translation.x as f32, cam_translation.y as f32, 0.);
+	}
+}
--- src/bevy/ui/camtype.rs
+++ src/bevy/ui/camtype.rs
@@ -1,26 +1,69 @@
 use crate::utils::*;
 
-#[allow(private_bounds)]
 #[derive(Component, Debug, Default)]
-pub struct UiCamera<S: CamSticky>(PhantomData<S>);
+pub struct UiCamera<S: CamType>(PhantomData<S>);
 
 assert_impl_all!(UiCamera<BottomLeft>: Send, Sync);
 
-trait CamSticky: Component + std::fmt::Debug + Send + Sync + Default {}
+#[allow(private_bounds)]
+pub trait CamType: Component + std::fmt::Debug + Send + Sync + Default + Sealed {
+	fn get_cam_transform(half_width: f32, half_height: f32) -> UVec2;
+
+	/// Just a large number to stop cameras from viewing the same area
+	/// When Bevy releases a proper SubApp or Multi-World functionality, that will be used instead
+	fn get_non_interfering_offset() -> Vec2;
+
+	fn get_camera_order() -> isize;
+}
+trait Sealed {}
 
 macro_rules! impl_cam_sticky {
-		($(pub struct $name:ident;)*) => {
+		($(pub struct $name:ident; half_width = $w:literal%, half_height = $h:literal %, offset = ($x:literal, $y:literal), order = $order:literal)*) => {
+		// ($(pub struct $name:ident;)*) => {
 			$(
 				#[derive(Component, Debug, Clone, Copy, Default)]
 				pub struct $name;
-				impl CamSticky for $name {}
+				impl Sealed for $name {}
+				impl CamType for $name {
+					fn get_cam_transform(half_width: f32, half_height: f32) -> UVec2 {
+						UVec2::new(
+							(half_width as f32 * $w as f32 / 100.) as u32,
+							(half_height as f32 * $h as f32 / 100.) as u32,
+						)
+					}
+
+					fn get_non_interfering_offset() -> Vec2 {
+						Vec2::new($x as f32, $y as f32)
+					}
+
+					fn get_camera_order() -> isize {
+						$order
+					}
+				}
 			)*
 		};
 }
 
 impl_cam_sticky!(
-	pub struct BottomLeft;
-	pub struct TopLeft;
-	pub struct TopRight;
-	pub struct BottomRight;
+	pub struct BottomLeft; half_width = 100%, half_height = 100%, offset = (0, 0), order = 1
+	pub struct TopLeft; half_width = 100%, half_height = -100%, offset = (1000, 0), order = 2
+	pub struct BottomRight; half_width = -100%, half_height = 100%, offset = (2000, 0), order = 3
+	pub struct TopRight; half_width = -100%, half_height = -100%, offset = (3000, 0), order = 4
 );
+
+impl<T: CamType> UiCamera<T> {
+	pub fn get_camera_bundle() -> impl Bundle {
+		Camera2dBundle {
+			camera: Camera {
+				order: T::get_camera_order(),
+				..default()
+			},
+			camera_2d: Camera2d {
+				clear_color: ClearColorConfig::None,
+			},
+			..default()
+		}
+		.insert(Self::default())
+		.not_pickable()
+	}
+}
