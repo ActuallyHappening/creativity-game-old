a1aa96112e041b98ecc56bebcfeb0d8b6b888367
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-10-29 15:43:48 +1000

    fix: btns are slightly better

--- src/bevy/ui/startscreen.rs
+++ src/bevy/ui/startscreen.rs
@@ -3,7 +3,9 @@ use crate::utils::*;
 pub struct StartScreenPlugin;
 impl Plugin for StartScreenPlugin {
 	fn build(&self, app: &mut App) {
-		app.add_systems(OnEnter(ScreenState::StartScreen), (setup_ui)).add_systems(OnExit(ScreenState::StartScreen), (cleanup_ui));
+		app
+			.add_systems(OnEnter(ScreenState::StartScreen), (setup_ui))
+			.add_systems(OnExit(ScreenState::StartScreen), (cleanup_ui));
 	}
 }
 
@@ -12,35 +14,50 @@ struct StartScreenUi;
 
 fn setup_ui(mut commands: Commands, ass: ResMut<AssetServer>) {
 	info!("Start screen");
-	commands.spawn(NodeBundle {
-		style: Style {
-			width: Val::Percent(100.),
-			height: Val::Percent(100.),
-			justify_content: JustifyContent::Center,
-			..default()
-		},
-		..default()
-	}).insert(StartScreenUi).with_children(|parent| {
-		parent.spawn(ButtonBundle {
+	commands
+		.spawn(NodeBundle {
 			style: Style {
-				width: Val::Px(200.),
-				height: Val::Px(60.),
+				width: Val::Percent(100.),
+				height: Val::Percent(100.),
+				justify_content: JustifyContent::Center,
+				align_items: AlignItems::Center,
+				flex_direction: FlexDirection::Row,
 				..default()
 			},
 			..default()
-		}).with_children(|parent| {
-			parent.spawn(TextBundle::from_section(
-				"Start (Hosted) Game",
-				TextStyle {
-					font: ass.load(Font::Medium),
-					font_size: 40.,
-					color: Color::WHITE,
-				}
-			));
+		})
+		.insert(StartScreenUi)
+		.with_children(|parent| {
+			let btn = |name: &'static str, parent: &mut ChildBuilder| {
+				parent
+					.spawn(ButtonBundle {
+						style: Style {
+							width: Val::Px(400.),
+							height: Val::Px(50.),
+							justify_content: JustifyContent::Center,
+							align_items: AlignItems::Center,
+							..default()
+						},
+						background_color: Color::BLACK.into(),
+						..default()
+					})
+					.with_children(|parent| {
+						parent.spawn(TextBundle::from_section(
+							name,
+							TextStyle {
+								font: ass.load(Font::Medium),
+								font_size: 30.,
+								color: Color::WHITE,
+							},
+						));
+					});
+			};
+			btn("Start (Hosted) Game", parent);
+			btn("Start Private Game", parent);
+			btn("Join hosted Game", parent);
 		});
-	});
 }
 
-fn cleanup_ui(mut commands: Commands, ) {
-
+fn cleanup_ui(mut commands: Commands, ui: Query<Entity, With<StartScreenUi>>) {
+	commands.entity(ui.single()).despawn_recursive();
 }
