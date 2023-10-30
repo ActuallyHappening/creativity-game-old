856b30674e1f718a072be99c313447228b48d69c
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-10-30 21:12:45 +1000

    todo: debug

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
-// use bevy::{log::LogPlugin, prelude::*, window::WindowMode};
-// use creativity_game::*;
+use bevy::{log::LogPlugin, prelude::*, window::WindowMode};
+use creativity_game::*;
 
-// fn main() {
-// 	let mut app = App::new();
+fn main() {
+	let mut app = App::new();
 
-// 	app
-// 		.add_plugins(
-// 			DefaultPlugins
-// 				.set(WindowPlugin {
-// 					primary_window: Some(Window {
-// 						fit_canvas_to_parent: true,
-// 						prevent_default_event_handling: false,
-// 						canvas: Some("#canvas".to_string()),
-// 						title: "Creativity Game".to_string(),
-// 						mode: WindowMode::BorderlessFullscreen,
-// 						..default()
-// 					}),
-// 					..default()
-// 				})
-// 				.set(LogPlugin {
-// 					level: bevy::log::Level::WARN,
-// 					filter: "creativity_game=trace,bevy_ecs=info".into(),
-// 				})
-// 				.build(),
-// 		)
-// 		.add_plugins(MainPlugin)
-// 		.run();
-// }
+	app
+		.add_plugins(
+			DefaultPlugins
+				.set(WindowPlugin {
+					primary_window: Some(Window {
+						fit_canvas_to_parent: true,
+						prevent_default_event_handling: false,
+						canvas: Some("#canvas".to_string()),
+						title: "Creativity Game".to_string(),
+						mode: WindowMode::BorderlessFullscreen,
+						..default()
+					}),
+					..default()
+				})
+				.set(LogPlugin {
+					level: bevy::log::Level::WARN,
+					filter: "creativity_game=trace,bevy_ecs=info".into(),
+				})
+				.build(),
+		)
+		.add_plugins(MainPlugin)
+		.run();
+}
 
 //! A simple demo to showcase how player could send inputs to move the square and server replicates position back.
 //! Also demonstrates the single-player and how sever also could be a player.
@@ -65,7 +65,7 @@ impl Plugin for SimpleBoxPlugin {
     fn build(&self, app: &mut App) {
         app.replicate::<PlayerPosition>()
             .replicate::<PlayerColor>()
-            .add_client_event::<MoveDirection>(EventType::Ordered)
+            .add_client_event::<MoveDirection>(SendPolicy::Ordered)
             .add_systems(
                 Startup,
                 (
